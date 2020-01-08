19bd2be603a62988b554898de5be7ba36bba9707
javaspgr <huroot123>
2020-01-08 22:09:54 +0800

    新增注释

--- src/main/java/com/offcn/SpringbootJpaApplication.java
+++ src/main/java/com/offcn/SpringbootJpaApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootJpaApplication {
 
+    /**
+     * 新增注释
+     * @param args
+     */
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJpaApplication.class, args);
     }
